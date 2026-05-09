4d96a046612a40a1f5bbe599a3b84709603c59ce
Afeez Lawal <dazeefacreative@gmail.com>
2026-05-09 18:59:44 +0100

    May 9

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -14,10 +14,13 @@ export default function Pricing() {
         <div className="max-w-6xl mx-auto">
           <div className="max-w-4xl mx-auto text-center mb-16">
             <h2 className={`max-w-3xl mx-auto text-4xl font-poltawski font-bold mb-4 ${isDark ? 'text-secondary' : 'text-gray-900'}`}>
-              Choose and purchase the plan that best suits your needs.
+              Start free. Upgrade when you grow.
             </h2>
-            <p className={`max-w-4xl ${isDark ? 'text-gray-300' : 'text-gray-700'}`}>
-           Choose a plan that fits your current stage and goals. As you grow, upgrading unlocks more capacity, deeper insights, and tools to help you increase engagement and scale your usage.<br/>Each plan is designed to help you turn every QR interaction into a more valuable experience.
+            <p className={`max-w-4xl text-sm ${isDark ? 'text-gray-300' : 'text-gray-700'}`}>
+           Choose a plan that fits your current stage and goals. As you grow, upgrading unlocks more capacity, deeper insights, branding, and tools to help you increase engagement and scale your usage.<br/>Each plan is designed to help you turn every QR interaction into a more valuable experience.
+            </p>
+            <p className={`max-w-4xl text-sm mt-6 font-bold ${isDark ? 'text-gray-300' : 'text-gray-700'}`}>
+            You get free access to all features for 30 days on your first signup. No credit card required.
             </p>
           </div>
           <div className="scale-70">
